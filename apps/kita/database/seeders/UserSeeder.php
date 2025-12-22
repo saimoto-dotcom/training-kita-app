bdd327557e50ec3dba002df8dec21f8acbac391e
Shuji Saimoto <saimoto@craid-inc.com>
2025-12-22 11:54:47 +0900

    “Seeder作成”

--- apps/kita/database/seeders/UserSeeder.php
+++ apps/kita/database/seeders/UserSeeder.php
@@ -2,20 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run(): void
     {
-        $this->call([
-            UserSeeder::class,
-            ArticleSeeder::class,
+        User::factory()->create([
+            'email' => 'test@example.com',
         ]);
     }
 }
