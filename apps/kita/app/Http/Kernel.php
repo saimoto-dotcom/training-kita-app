11bc746b4fc7f347d0f6212a34614dda9778e120
Shuji Saimoto <saimoto@craid-inc.com>
2026-01-05 17:40:21 +0900

    “fix:PR指摘修正漏れ反映”

--- apps/kita/app/Http/Kernel.php
+++ apps/kita/app/Http/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            // ★ ここが最重要。StartSession よりも前に実行させる
+            // StartSession よりも前に実行させる
             \App\Http\Middleware\SetAdminSessionCookie::class,
 
             \App\Http\Middleware\EncryptCookies::class,
